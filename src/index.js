c79833d3c2f375d9960bf05642c516051ce8ae58
Messalf <tagliafero.massimo@gmail.com>
2019-08-08 14:08:29 +0200

    happydrinkdone

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React    from 'react';
+import ReactDOM from 'react-dom';
+import App      from './components/App';
+import './css/index.css';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App title = "HappyDrink" />, document.getElementById('root'));
+// On utilise la méthode render() de ReactDOM pour décrire DOM ,
+// le premier paramètre correspond à ce que l'on veut rendre
+// et le deuxième l'endroit où "accrocher" le DOM que l'on crée.
+//      ici : l'élément d'id root dans le fichier ./public/index.html
+serviceWorker.unregister();
